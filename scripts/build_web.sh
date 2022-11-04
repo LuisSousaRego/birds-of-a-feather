ded01bad8c0107561378ad1da68fce5b9b3cb400
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2022-11-04 16:59:20 +0100

    add built web version

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -1,2 +1,2 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./web/ --target web ./target/
+wasm-bindgen --out-dir ./web/ --target web ./target/wasm32-unknown-unknown/release/birds-of-a-feather.wasm
